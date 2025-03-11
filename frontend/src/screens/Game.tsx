ffb5aa9eed24e99fae0e340c2d7368be2bee95fc
Your Name <barotjay998@yahoo.com>
2025-03-11 12:32:24 -0500

    frontend

---
+++ frontend/src/screens/Game.tsx
@@ -0,0 +1,5 @@
+export const Game = () => {
+    return <div>
+        <h1>Game Page</h1>
+    </div>
+}
---
+++ frontend/src/screens/Landing.tsx
@@ -0,0 +1,5 @@
+export const Landing = () => {
+    return <div>
+        <h1>Landing Page</h1>
+    </div>
+}
